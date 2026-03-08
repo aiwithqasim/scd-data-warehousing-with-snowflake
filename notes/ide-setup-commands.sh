64f1ae0e6ef9f53260edd2290629b855f4c6c768
aiwithqasim <qasimhassan1020@gmail.com>
2026-03-09 02:16:17 +0500

    task(update): updating nifi setup material

--- notes/ide-setup-commands.sh
+++ notes/ide-setup-commands.sh
@@ -1,8 +1,8 @@
 # Commands to connect to the EC2 instance and transfer files
 
-ssh -i "kp-defualt-ah.pem" ec2-user@ec2-35-92-63-30.us-west-2.compute.amazonaws.com
-ssh -i "kp-defualt-ah.pem" ec2-user@ec2-35-92-63-30.us-west-2.compute.amazonaws.com -L 2081:localhost:2041 -L 4888:localhost:4888 -L 2080:localhost:2080 -L 8050:localhost:8050 -L 4141:localhost:4141
-scp -r -i "kp-defualt-ah.pem" docker_exp ec2-user@ec2-35-92-63-30.us-west-2.compute.amazonaws.com:/home/ec2-user/docker_exp
+ssh -i "kp-default-qh.pem" ec2-user@ec2-44-255-172-150.us-west-2.compute.amazonaws.com
+scp -r -i "kp-default-qh.pem" docker_exp ec2-user@ec2-44-255-172-150.us-west-2.compute.amazonaws.com:/home/ec2-user/docker_exp
+ssh -i "kp-default-qh.pem" ec2-user@ec2-44-255-172-150.us-west-2.compute.amazonaws.com -L 2081:localhost:2041 -L 4888:localhost:4888 -L 2080:localhost:2080 -L 8050:localhost:8050 -L 4141:localhost:4141
 
 # Installing docker and docker-compose on the EC2 instance
 sudo yum update -y
@@ -23,3 +23,6 @@ docker exec -i -t nifi bash
 
 # Jupyter Lab at: http://localhost:4888/lab? 
 # NiFi at: http://localhost:2080/nifi/ 
+
+docker exec -i -t nifi bash
+cd /opt/workspace/nifi/data/
